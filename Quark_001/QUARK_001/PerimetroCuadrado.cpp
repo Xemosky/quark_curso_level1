40b71259b668a192a2c172771a4c44171cf0ff1e
Jose Miguel <jmibarrav@gmail.com>
2023-02-14 11:21:19 -0300

    primer commit - Prueba 1

---
+++ Quark_001/QUARK_001/PerimetroCuadrado.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+/*
+int main()
+{
+    int num1, perimetro;
+    cout << "Ingrese lado de cuadrado: ";
+    cin >> num1;
+    perimetro = num1 * 4;
+    cout << "El perimetro del cuadrado es:";
+    cout << perimetro;
+
+    return 0;
+}
+
+*/
---
+++ Quark_001/QUARK_001/SumaProducto.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// Initialization of base class
+class Base {
+public:
+    virtual ~Base() = 0; // Pure virtual destructor
+};
+Base::~Base() // Explicit destructor call
+{
+    std::cout << "Pure virtual destructor is called";
+}
+
+// Initialization of derived class
+class Derived : public Base {
+public:
+    ~Derived() { std::cout << "~Derived() is executed\n"; }
+};
+
+int main()
+{
+    // Calling of derived member function
+    Base* b = new Derived();
+    delete b;
+    return 0;
+}
